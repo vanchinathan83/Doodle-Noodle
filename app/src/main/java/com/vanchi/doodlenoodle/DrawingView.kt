74db28930ad43c66d710a9398cc05269c63ac6d1
vayipalayamchand <vayipalayamchand@paypal.com>
2021-11-07 19:39:54 -0600

    Initial commit with Canvas and Brush configs
    Added a dialog to show the brush sizes
    Added canvas with Bitmap and peristed the drawings.

---
+++ app/src/main/java/com/vanchi/doodlenoodle/DrawingView.kt
@@ -0,0 +1,96 @@
+package com.vanchi.doodlenoodle
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var mColor = Color.BLUE
+    private var mCanvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(mColor, mBrushSize)
+        mDrawPaint!!.color = mColor
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+
+    }
+
+    fun setBrushSize(brushThickness: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        brushThickness, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
+
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
+
+    }
+
+    // When the screen is created, we create the bitmap and assign it to the canvas
+    // Canvas has the bitmap
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888)
+        mCanvas = Canvas(mCanvasBitmap!!)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!,0f, 0f, mCanvasPaint)
+        for (path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color  = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+        if(!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color  = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = mColor
+                mDrawPath!!.brushThickness = mBrushSize
+                mDrawPath!!.reset()
+                if(touchX != null && touchY != null) {
+                    mDrawPath!!.moveTo(touchX, touchY)
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if(touchX != null && touchY != null) {
+                    mDrawPath!!.lineTo(touchX, touchY)
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
+                mDrawPath = CustomPath(mColor, mBrushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+}
---
+++ app/src/main/java/com/vanchi/doodlenoodle/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.vanchi.doodlenoodle
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import com.vanchi.doodlenoodle.databinding.ActivityMainBinding
+import com.vanchi.doodlenoodle.databinding.DialogBrushSizeBinding
+
+class MainActivity : AppCompatActivity() {
+    private var drawingView: DrawingView? = null
+    private var binding : ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        drawingView = binding!!.drawingView
+        drawingView!!.setBrushSize(30.toFloat())
+        binding?.paintBrush?.setOnClickListener { view ->
+            showBrushSizeDialog()
+        }
+    }
+
+    fun showBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        val brushDialogBinding: DialogBrushSizeBinding = DialogBrushSizeBinding.inflate(layoutInflater)
+        brushDialog.setContentView(brushDialogBinding.root)
+        val smallBrush = brushDialogBinding.smallBrush
+        val mediumBrush = brushDialogBinding.mediumBrush
+        val largeBrush = brushDialogBinding.largeBrush
+
+        smallBrush.setOnClickListener { view ->
+            drawingView?.setBrushSize(20.toFloat())
+            brushDialog.dismiss()
+        }
+        mediumBrush.setOnClickListener { view ->
+            drawingView?.setBrushSize(25.toFloat())
+            brushDialog.dismiss()
+        }
+        largeBrush.setOnClickListener { view ->
+            drawingView?.setBrushSize(30.toFloat())
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+    }
+}
